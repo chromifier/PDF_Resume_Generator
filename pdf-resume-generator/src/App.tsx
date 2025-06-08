27f6d4f7a2fd8e2f51c7beffef804f989fe72a21
Jeff <jmulkinj@gmail.com>
2025-06-07 19:38:41 -0700

    Initial Vite create, header creation begins

---
+++ pdf-resume-generator/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="header" className="top-0 sticky bg-emerald-200 p-8">
+        <h1 className="text-3xl text-center font-bold text-gray-800">
+          PDF Resume Generator
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
